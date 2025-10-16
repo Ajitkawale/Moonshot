b07f0c03352e817c15a7b7bdc471dc7b7e96fa7e
Ajit Kawale <ajitkawale27@gmail.com>
2025-10-16 19:28:55 +0530

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Ajit Kawale on 16/10/25.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
